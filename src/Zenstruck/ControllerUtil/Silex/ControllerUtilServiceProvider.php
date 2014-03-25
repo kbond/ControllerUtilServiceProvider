aff45cdfa700edeb64da491da0c07044a4e68a5f
Kevin Bond <kevinbond@gmail.com>
2014-03-25 12:48:29 -0400

    broke apart large method

--- src/Zenstruck/ControllerUtil/Silex/ControllerUtilServiceProvider.php
+++ src/Zenstruck/ControllerUtil/Silex/ControllerUtilServiceProvider.php
@@ -5,7 +5,6 @@ namespace Zenstruck\ControllerUtil\Silex;
 use JMS\Serializer\SerializerInterface;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -33,65 +32,68 @@ class ControllerUtilServiceProvider implements ServiceProviderInterface
      */
     public function boot(Application $app)
     {
-        /** @var EventDispatcher $eventDispatcher */
-        $eventDispatcher = $app['dispatcher'];
+        $this->registerForwardListener($app);
+        $this->registerRedirectListener($app);
+        $this->registerHasFlashesListener($app);
+        $this->registerTwigViewListener($app);
+        $this->registerSerializerViewListener($app);
+    }
 
-        /** @var UrlGeneratorInterface $urlGenerator */
-        $urlGenerator = isset($app['url_generator']) && $app['url_generator'] instanceof UrlGeneratorInterface
-            ? $app['url_generator']
-            : null
-        ;
-
-        /** @var Session $session */
-        $session = isset($app['session']) && $app['session'] instanceof Session
-            ? $app['session']
-            : null
-        ;
-
-        /** @var \Twig_Environment $twig */
-        $twig = isset($app['twig']) && $app['twig'] instanceof \Twig_Environment
-            ? $app['twig']
-            : null
-        ;
-
-        $serializer = isset($app['serializer']) && $app['serializer'] instanceof SerializerInterface
-            ? $app['serializer']
-            : null
-        ;
-
-        $eventDispatcher->addListener(
+    private function registerForwardListener(Application $app)
+    {
+        $app['dispatcher']->addListener(
             KernelEvents::VIEW,
             array(new ForwardListener(), 'onKernelView')
         );
+    }
 
-        if ($urlGenerator) {
-            $eventDispatcher->addListener(
-                KernelEvents::VIEW,
-                array(new RedirectListener($urlGenerator), 'onKernelView')
-            );
+    private function registerRedirectListener(Application $app)
+    {
+        if (!isset($app['url_generator']) || !$app['url_generator'] instanceof UrlGeneratorInterface) {
+            return;
         }
 
-        if ($session) {
-            $eventDispatcher->addListener(
-                KernelEvents::VIEW,
-                array(new HasFlashesListener($session->getFlashBag()), 'onKernelView'),
-                10 // before other events
-            );
+        $app['dispatcher']->addListener(
+            KernelEvents::VIEW,
+            array(new RedirectListener($app['url_generator']), 'onKernelView')
+        );
+    }
+
+    private function registerHasFlashesListener(Application $app)
+    {
+        if (!isset($app['session']) || !$app['session'] instanceof Session) {
+            return;
         }
 
-        if ($twig) {
-            $eventDispatcher->addListener(
-                KernelEvents::VIEW,
-                array(new TwigViewListener($twig), 'onKernelView')
-            );
+        $app['dispatcher']->addListener(
+            KernelEvents::VIEW,
+            array(new HasFlashesListener($app['session']->getFlashBag()), 'onKernelView'),
+            10 // before other events
+        );
+    }
+
+    private function registerTwigViewListener(Application $app)
+    {
+        if (!isset($app['twig']) || !$app['twig'] instanceof \Twig_Environment) {
+            return;
         }
 
-        if ($serializer) {
-            $eventDispatcher->addListener(
-                KernelEvents::VIEW,
-                array(new SerializerViewListener($serializer), 'onKernelView'),
-                5
-            );
+        $app['dispatcher']->addListener(
+            KernelEvents::VIEW,
+            array(new TwigViewListener($app['twig']), 'onKernelView')
+        );
+    }
+
+    private function registerSerializerViewListener(Application $app)
+    {
+        if (!isset($app['serializer']) || !$app['serializer'] instanceof SerializerInterface) {
+            return;
         }
+
+        $app['dispatcher']->addListener(
+            KernelEvents::VIEW,
+            array(new SerializerViewListener($app['serializer']), 'onKernelView'),
+            5 // before other events
+        );
     }
 }
